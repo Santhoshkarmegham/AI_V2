3e9c175aaf6c3a49bca73cc574db5129ca2dd1fc
Santhoshkarmegham <vksan131@gmail.com>
2026-06-22 19:48:48 +0100

    main url updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -57,6 +57,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5176",
         "http://127.0.0.1:5176",
+        "https://ai-v2-omega.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
